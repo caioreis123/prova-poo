f6d6322eae318d7756f491656b6836f8620e9f1e
caio reis <reis.caio@gmail.com>
2021-11-07 00:17:35 -0300

    finaliza questão 1

--- questao-1/src/ConversaoDeUnidadesDeArea.java
+++ questao-1/src/ConversaoDeUnidadesDeArea.java
@@ -1,2 +1,21 @@
 public class ConversaoDeUnidadesDeArea {
+    public static float metroParaPe(float metro) {
+        return (metro * 10.76f);
+    }
+
+    public static float peParaCentimetro(float pe) {
+        return (pe * 929);
+    }
+
+    public static float milhaParaAcre(float milha) {
+        return (milha * 640);
+    }
+
+    public static float acreParaPe(float acre) {
+        return (acre * 43560);
+    }
+
+    public static float metroParaAcre(float metro) {
+        return (metro * 0.000247105f);
+    }
 }
--- questao-1/src/Main.java
+++ questao-1/src/Main.java
@@ -1,5 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Caio!");
+        System.out.println("Digite a área do campo de futebol em metros quadrados\n");
+        BufferedReader leitor = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            float metros = Float.parseFloat(leitor.readLine());
+            float pes = ConversaoDeUnidadesDeArea.metroParaPe(metros);
+            float centimetros = ConversaoDeUnidadesDeArea.peParaCentimetro(pes);
+            float acres = ConversaoDeUnidadesDeArea.metroParaAcre(metros);
+            System.out.printf("A área do campo de futebol é de: %f pes quadrados, %f acres quadrados e %f centimetros quadrados%n", pes, acres, centimetros);
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
     }
 }
